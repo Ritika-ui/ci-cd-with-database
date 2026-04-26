6941722f620929c92c0d5fecc68ff1acebcddaf2
Ritika Khadka <ritikakhadka155@gmail.com>
2026-04-26 01:51:51 +0000

    ready for deploy

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes.tasks import tasks_bp
+import os 
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def home():
     return "Task Manager API is running 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
